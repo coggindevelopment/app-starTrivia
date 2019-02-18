4d12b0efb6f6e5cb037e64000721a9105fe54764
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-02-18 06:24:18 -0600

    initial commit - project creation and setup

---
+++ StarTrivia/StarTrivia/SelectPersonVC.swift
@@ -0,0 +1,20 @@
+//
+//  SelectPersonVC.swift
+//  StarTrivia
+//
+//  Created by Nicholas Coggin on 2/18/19.
+//  Copyright © 2019 Nicholas Coggin. All rights reserved.
+//
+
+import UIKit
+
+class SelectPersonVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
