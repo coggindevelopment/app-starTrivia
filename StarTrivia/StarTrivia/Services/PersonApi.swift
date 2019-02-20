a26db5efd1cb9421dcea54373fba6fb0275124f5
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-02-20 05:16:05 -0600

    lesson-07 commit -- URLSession

---
+++ StarTrivia/StarTrivia/Services/PersonApi.swift
@@ -0,0 +1,36 @@
+//
+//  PersonApi.swift
+//  StarTrivia
+//
+//  Created by Nicholas Coggin on 2/20/19.
+//  Copyright © 2019 Nicholas Coggin. All rights reserved.
+//
+
+import Foundation
+
+class PersonApi {
+    func getRandomPersonUrlSession() {
+        
+        guard let url = URL(string: PERSON_URL) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard error == nil else {
+                debugPrint(error.debugDescription)
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let jsonAny = try JSONSerialization.jsonObject(with: data, options: [])
+                guard let json = jsonAny as? [String: Any] else { return }
+                print(json)
+            } catch {
+                debugPrint(error.localizedDescription)
+                return
+            }
+        }
+        task.resume()
+    }
+}
