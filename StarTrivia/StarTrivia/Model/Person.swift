9144f65066b9a86657fa4b556f1ca2af5c706dc6
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-02-20 05:39:57 -0600

    lesson-08 -- Parsing JSON Manually

---
+++ StarTrivia/StarTrivia/Model/Person.swift
@@ -0,0 +1,22 @@
+//
+//  Person.swift
+//  StarTrivia
+//
+//  Created by Nicholas Coggin on 2/20/19.
+//  Copyright © 2019 Nicholas Coggin. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let height: String
+    let mass: String
+    let hair: String
+    let birthYear: String
+    let gender: String
+    let homeworldUrl: String
+    let filmUrls: [String]
+    let vehicleUrls: [String]
+    let starshipUrls: [String]
+}
