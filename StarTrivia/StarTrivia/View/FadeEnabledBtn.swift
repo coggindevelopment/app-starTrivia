971f8bf7a0fc016250bbf850346d183b303a3ce8
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-02-27 06:05:48 -0600

    lesson-17 commit -- FadeEnabledButton

---
+++ StarTrivia/StarTrivia/View/FadeEnabledBtn.swift
@@ -0,0 +1,26 @@
+//
+//  FadeEnabledBtn.swift
+//  StarTrivia
+//
+//  Created by Nicholas Coggin on 2/27/19.
+//  Copyright © 2019 Nicholas Coggin. All rights reserved.
+//
+
+import UIKit
+
+class FadeEnabledBtn : UIButton {
+    
+    override var isEnabled: Bool {
+        didSet {
+            if isEnabled {
+                UIView.animate(withDuration: 0.2) {
+                    self.alpha = 1.0
+                }
+            } else {
+                UIView.animate(withDuration: 0.2) {
+                    self.alpha = 0.5
+                }
+            }
+        }
+    }
+}
