04bb049786a42af63f6e08cc896e3b7ca153b15c
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-03-01 05:11:50 -0600

    lesson-22 commit -- Vehicles UI

--- StarTrivia/StarTrivia/Controller/VehiclesVC.swift
+++ StarTrivia/StarTrivia/Controller/VehiclesVC.swift
@@ -10,24 +10,32 @@ import UIKit
 
 class VehiclesVC: UIViewController, PersonProtocol {
     
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var modelLbl: UILabel!
+    @IBOutlet weak var manufacturerLbl: UILabel!
+    @IBOutlet weak var costLbl: UILabel!
+    @IBOutlet weak var lengthLbl: UILabel!
+    @IBOutlet weak var SpeedLbl: UILabel!
+    @IBOutlet weak var crewLbl: UILabel!
+    @IBOutlet weak var passengersLbl: UILabel!
+    @IBOutlet weak var previousBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    
     var person: Person!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print(person.name)
 
-        // Do any additional setup after loading the view.
+        
+    }
+
+    @IBAction func previousClicked(_ sender: Any) {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func nextClicked(_ sender: Any) {
+        
     }
-    */
-
 }
