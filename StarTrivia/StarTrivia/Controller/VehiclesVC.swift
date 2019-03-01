3c5e75d14095cf9b2b115793213a6cd04a18fee8
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-03-01 05:56:40 -0600

    lesson-23 commit -- VehiclesAPI

--- StarTrivia/StarTrivia/Controller/VehiclesVC.swift
+++ StarTrivia/StarTrivia/Controller/VehiclesVC.swift
@@ -15,19 +15,41 @@ class VehiclesVC: UIViewController, PersonProtocol {
     @IBOutlet weak var manufacturerLbl: UILabel!
     @IBOutlet weak var costLbl: UILabel!
     @IBOutlet weak var lengthLbl: UILabel!
-    @IBOutlet weak var SpeedLbl: UILabel!
+    @IBOutlet weak var speedLbl: UILabel!
     @IBOutlet weak var crewLbl: UILabel!
     @IBOutlet weak var passengersLbl: UILabel!
     @IBOutlet weak var previousBtn: UIButton!
     @IBOutlet weak var nextBtn: UIButton!
     
-    
     var person: Person!
+    let api = VehicleApi()
+    var vehicles = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(person.name)
-
+        vehicles = person.vehicleUrls
+        guard let firstVehicle = vehicles.first else { return }
+        getVehicle(url: firstVehicle)
+        
+    }
+    
+    func getVehicle(url: String) {
+        api.getVehicle(url: url) { (vehicle) in
+            if let vehicle = vehicle {
+                self.setupView(vehicle: vehicle)
+            }
+        }
+    }
+    
+    func setupView(vehicle: Vehicle) {
+        nameLbl.text = vehicle.name
+        modelLbl.text = vehicle.model
+        manufacturerLbl.text = vehicle.manufacturer
+        costLbl.text = vehicle.cost
+        lengthLbl.text = vehicle.length
+        speedLbl.text = vehicle.speed
+        crewLbl.text = vehicle.crew
+        passengersLbl.text = vehicle.passengers
         
     }
 
---
+++ StarTrivia/StarTrivia/Model/Vehicle.swift
@@ -0,0 +1,31 @@
+//
+//  Vehicle.swift
+//  StarTrivia
+//
+//  Created by Nicholas Coggin on 3/1/19.
+//  Copyright © 2019 Nicholas Coggin. All rights reserved.
+//
+
+import Foundation
+
+struct Vehicle : Codable {
+    let name: String
+    let model: String
+    let manufacturer: String
+    let cost: String
+    let length: String
+    let speed: String
+    let crew: String
+    let passengers: String
+    
+    enum CodingKeys : String, CodingKey {
+        case name
+        case model
+        case manufacturer
+        case cost = "cost_in_credits"
+        case length
+        case speed = "max_atmosphere_speed"
+        case crew
+        case passengers
+    }
+}
